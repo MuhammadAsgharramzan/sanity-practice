86e9f0de9ae6262cd08c92edbdf103677a75cd1c
Muhammad Asghar <[asgharusa@gmail.com]>
2025-01-29 15:46:20 +0500

    third commit

--- src/app/components/hero/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Hero (){
-    return (
-        <div>
-            <p className="text-gray-900"> Welcome to Arsh store where you can find multiple Gadgets and house hold items </p>
-        </div>
-    )
-}
